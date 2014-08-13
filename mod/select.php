19e5f2428e8b2ffab2bb2e2e1e573ad20c8c997d
aishwary <rajaishwary@gmail.com>
2014-08-14 04:29:33 +0530

    select nahi ho raha .
    :(

---
+++ mod/select.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rajaishwary
+ * Date: 8/14/14
+ * Time: 2:36 AM
+ */
+try{
+      $objDb = new PDO ('sqlite:../dbase/shopping-list');
+      $objDb->setAttribute(PDO::ATTR_ERRMODE, PDO:: ERRMODE_EXCEPTION );
+
+        $sql = "SELECT `i`.*,
+                `t`.`name` AS `type_name`
+                FROM `items` `i`
+                JOIN `types` `t`
+                   ON `t`.`id` = `i`.`type`
+                   ORDER BY `i`.`date` ASC";
+
+         $result = $objDb -> query($sql);
+
+          if (!result){
+              throw new PDOExeption('the result returned no object');
+          }
+
+          $result->setFetchMode(PDO::FETCH_ASSOC);
+    $items = $result->fetchAll();
+
+    $sql = " SELECT *
+    FROM `types`
+    ORDER BY `id`";
+
+    $result = $objDb-> query($sql);
+    if (!result){
+        throw new PDOExeption('the result returned no object');
+    }
+
+    $result->setFetchMode(PDO::FETCH_ASSOC);
+
+    $types =  $result ->fetchAll();
+    echo json_encode(array(
+        'error' =>false,
+        'items' => $items,
+        'types' => $types
+    ),JSON_HEX_TAG | JSON_HEX_APOS | JSON_HEX_QUOT | JSON_HEX_AMP);
+
+
+} catch(PDOException $e){
+    echo json_encode(array(
+         'error' => true,
+         'message' => $e -> getMessage()
+    ),JSON_HEX_TAG | JSON_HEX_APOS | JSON_HEX_QUOT | JSON_HEX_AMP);
+
+}
